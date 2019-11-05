5075fbf1374841130eb9805e9d2f2a399678b557
bgordon83 <ben.gordon1983@gmail.com>
2019-11-05 16:06:55 -0500

    adds auth and package-lock.json

---
+++ assets/scripts/auth/auth-ui.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const store = require('./store')
+
+const successMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').removeClass('failure')
+  $('#message').addClass('success')
+  $('form').trigger('reset')
+}
+
+const failureMessage = function (newText) {
+  $('#message').text(newText)
+  $('#message').addClass('failure')
+  $('#message').removeClass('success')
+  $('form').trigger('reset')
+}
+
+const onSignUpSuccess = () => {
+  successMessage('Signed up Successfully!')
+  $('#message').css('color', 'green')
+}
+
+const onSignUpFailure = () => {
+  failureMessage('Signed up Failed')
+  $('#message').css('color', 'red')
+}
+const onSignInSuccess = (responseData) => {
+  successMessage('Signed in Successfully')
+  store.user = responseData.user
+  $('#message').css('color', 'green')
+  // $('#sign-up, #sign-in').hide()
+  // $('#change-password').show()
+  // $('#sign-out').show()
+}
+
+const onSignInFailure = function () {
+  $('#message').css('color', 'red')
+  failureMessage('Sign-in Failure')
+}
+
+const onChangePasswordSuccess = function () {
+  successMessage('Changed password successfully')
+  $('#message').css('color', 'green')
+}
+
+const onChangePasswordFailure = function () {
+  $('#message').css('color', 'red')
+  failureMessage('Changed password Failed')
+}
+
+const onSignOutSuccess = function () {
+  successMessage('Sign-out successfully')
+  $('#message').css('color', 'green')
+  // $('#sign-up, #sign-in').show()
+  // $('#change-password').hide()
+  // $('#sign-out').hide()
+}
+
+const onSignOutFailure = function () {
+  failureMessage('Signed out failed')
+  $('#message').css('color', 'red')
+}
+module.exports = {
+  onSignUpSuccess,
+  onSignUpFailure,
+  onSignInSuccess,
+  onSignInFailure,
+  onChangePasswordSuccess,
+  onChangePasswordFailure,
+  onSignOutSuccess,
+  onSignOutFailure
+}
