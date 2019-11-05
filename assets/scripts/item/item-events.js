9bf4c5436223a31bc3ac2636d13f5133af09c641
aboborodea <aboborodea@gmail.com>
2019-11-05 16:05:47 -0500

    added crud actions for items in events and api still need to work on ui

--- assets/scripts/item/item-events.js
+++ assets/scripts/item/item-events.js
@@ -1,1 +1,54 @@
+const getFormFields = require('../../../lib/get-form-fields.js')
+const itemApi = require('./item-api.js')
+const itemUi = require('./item-ui.js')
 
+const onAddItem = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  itemApi.addItem(formData)
+    .then(itemUi.onAddItemSuccess)
+    .catch(itemUi.onAddItemFailure)
+}
+
+const onViewItem = function (event) {
+  event.preventDefault()
+
+  const form = event.target
+  const formData = getFormFields(form)
+  itemApi.viewItem(formData)
+    .then(itemUi.onViewItemSuccess)
+    .catch(itemUi.onViewItemFailure)
+}
+
+const onViewItems = function (event) {
+  event.preventDefault()
+  itemApi.viewItem()
+    .then(itemUi.onViewItemSuccess)
+    .catch(itemUi.onViewItemFailure)
+}
+
+const onDeleteItem = function (event) {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  itemApi.deleteItem(formData)
+    .then(itemUi.onDeleteItemSuccess)
+    .catch(itemUi.onDeleteItemFailure)
+}
+
+const onUpdateItem = function (event) {
+  event.preventDefault()
+  const formData = getFormFields(event.target)
+  itemApi.updateItem(formData)
+    .then(itemUi.onUpdateItemSuccess)
+    .catch(itemUi.onUpdateItemFailure)
+}
+
+module.exports = {
+  onAddItem,
+  onViewItem,
+  onViewItems,
+  onDeleteItem,
+  onUpdateItem
+}
