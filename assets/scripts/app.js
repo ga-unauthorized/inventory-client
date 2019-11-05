915e9370f51d65b2703874379b935fe62be5e580
RNolan19 <rnolan19@gmail.com>
2019-11-05 16:13:40 -0500

    Created app.js with HTML IDs

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,34 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./auth/auth-events')
+const itemEvents = require('./item/item-events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#change-password').on('submit', authEvents.onChangePassword)
+  $('#sign-out').on('submit', authEvents.onSignOut)
+
+  $('#add-item').on('submit', itemEvents.onAddItem)
+  $('#view-items').on('submit', itemEvents.onViewItems)
+  // make sure view-item being singular doesn't cause errors
+  $('#delete-item').on('submit', itemEvents.onDeleteItem)
+  $('#update-item').on('submit', itemEvents.onUpdateItem)
+  $('#view-item').on('submit', itemEvents.onViewItem)
+
+  $('#change-password').hide()
+  $('.h2changepassword').hide()
+  $('#sign-out').hide()
+  $('.h2signout').hide()
+  $('#add-run').hide()
+  $('.h2addrun').hide()
+  $('#view-runs').hide()
+  $('.h2viewruns').hide()
+  $('#delete-run').hide()
+  $('.h2deleterun').hide()
+  $('#update-run').hide()
+  $('.h2updaterun').hide()
 })
