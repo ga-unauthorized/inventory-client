4915e8c1890b67e1600084184c34510107a94641
JiemingS <sunjieming1121@gmail.com>
2019-11-07 17:21:01 -0500

    UPDATE the url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://quiet-oasis-34691.herokuapp.com/',
+  production: 'https://glacial-taiga-09846.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
