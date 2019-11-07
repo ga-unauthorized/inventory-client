17ae64ba55a70be3f2ac5af09f573f6847f9283a
RNolan19 <rnolan19@gmail.com>
2019-11-07 14:32:36 -0500

    Hid nav bar and buttons on main screen. In progress.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -22,7 +22,26 @@ $(() => {
   $('#update2-item').on('submit', itemEvents.onUpdateItem2)
   $('#view-item').on('submit', itemEvents.onViewItem)
 
-  // const qrcode = new QRCode('test', {
+  $('.navbar').hide()
+  $('.footer').hide()
+  $('.table').hide()
+  $('#view-items').hide()
+
+  $('#change-password').hide()
+  $('.h2changepassword').hide()
+
+  $('#sign-out').hide()
+  $('.h2signout').hide()
+  $('#add-run').hide()
+  $('.h2addrun').hide()
+  $('#view-runs').hide()
+  $('.h2viewruns').hide()
+  $('#delete-run').hide()
+  $('.h2deleterun').hide()
+  $('#update-run').hide()
+  $('.h2updaterun').hide()
+
+  // const qrcode = new QRCode ('test', {
   //   text: 'http://jindo.dev.naver.com/collie',
   //   width: 128,
   //   height: 128,
